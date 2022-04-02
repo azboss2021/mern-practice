1ab46abaff1a59f5afafd3ee1399786b8e50d8cb
azboss2021 <alexanderzyn1@gmail.com>
2022-04-02 13:29:07 -0700

    setting up react app repository

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>List products</main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
