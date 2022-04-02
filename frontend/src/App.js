2331a4ccc5e026fe678337e183f67f6759fac482
azboss2021 <alexanderzyn1@gmail.com>
2022-04-02 16:52:04 -0700

    create express backend/changed a tags to Link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">amazona</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="featured_products">
-          {data.products.map((product) => (
-            <div className="featured_product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="featured_product_info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-      <footer></footer>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+        <footer></footer>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+import data from '../data';
+
+function HomeScreen() {
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      <div className="featured_products">
+        {data.products.map((product) => (
+          <div className="featured_product" key={product.slug}>
+            <Link to={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </Link>
+            <div className="featured_product_info">
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+              <button>Add to cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+export default HomeScreen;
