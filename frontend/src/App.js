d9ca9eb684957ae29818fba3addd269d9a4ff5ed
azboss2021 <alexanderzyn1@gmail.com>
2022-04-02 14:06:43 -0700

    added product images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data';
 
 function App() {
   return (
@@ -7,7 +8,27 @@ function App() {
       <header>
         <a href="/">amazona</a>
       </header>
-      <main>List products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="featured_products">
+          {data.products.map((product) => (
+            <div className="featured_product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="featured_product_info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
       <footer></footer>
     </div>
   );
